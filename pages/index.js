048311085f9b01add6e74d0ed1a1add008c9f441
adam040992 <adam040992@gmail.com>
2021-11-11 13:11:23 +0100

    Added MapBox (config)

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
+import { useEffect } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css';
 import tw from "tailwind-styled-components";
+import mapboxgl from '!mapbox-gl';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbTA0MDk5MiIsImEiOiJja3Z1dnk0cjQwNHF0Mm9waDB5bTNrejVtIn0.8tVmWw0pmqy6naxWM-N0xQ';
 
 export default function Home() {
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
+  },[])
+  
   return (
     <Wrapper>
-      <Map>Map</Map>
+      <Map id='map'>Map</Map>
       <ActionItems>Start</ActionItems>
     </Wrapper>
   )
